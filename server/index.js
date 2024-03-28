fbca34034c68fedaf3418a476ad1c505f2189212
Utkarsh Pathak <utkarsh.pathak@optym.com>
2024-03-28 14:35:24 +0530

    updated paths

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import { adminRouter } from "./Routes/AdminRoute.js";
 const app=express()
 app.use(cors({
-    origin:["http://localhost:5174","http://localhost:5173"],
+    origin:["http://localhost:5174","http://localhost:5173","https://66052f42c35dec690b157a37--resplendent-gumption-7e156f.netlify.app"],
     methods:['GET','POST','PUT','DELETE'],
     credentials: true
 }))
